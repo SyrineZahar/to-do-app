b00b8459b7890e3ab003da2f0bb2e754638280e3
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 18:33:13 +0100

    Update TaskStatus.enum.ts

--- ToDoAppFront/src/app/classe/Enum/TaskStatus.enum.ts
+++ ToDoAppFront/src/app/classe/Enum/TaskStatus.enum.ts
@@ -1,5 +1,8 @@
+/**
+ * Énumération représentant les différents statuts d'une tâche.
+ */
 export enum TaskStatus{
-    todo="TODO",
-    inprogress="INPROGRESS",
-    done="DONE"
+    todo="TODO", // Statut pour une tâche à faire
+    inprogress="INPROGRESS",// Statut pour une tâche en cours
+    done="DONE" // Statut pour une tâche terminée
 }
