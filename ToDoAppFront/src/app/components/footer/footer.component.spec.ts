5a3ce17bb4e3ac3f9726392c3bf31b55b8c5510f
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 18:47:47 +0100

    Update footer.component.spec.ts

--- ToDoAppFront/src/app/components/footer/footer.component.spec.ts
+++ ToDoAppFront/src/app/components/footer/footer.component.spec.ts
@@ -1,21 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; // Importation des modules nécessaires pour les tests unitaires.
+import { FooterComponent } from './footer.component'; // Importation du composant Footer à tester.
 
-import { FooterComponent } from './footer.component';
+describe('FooterComponent', () => { // Début du bloc de tests pour le FooterComponent.
+  let component: FooterComponent; // Déclaration de la variable pour le composant.
+  let fixture: ComponentFixture<FooterComponent>; // Déclaration de la variable pour le fixture.
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+  beforeEach(() => { // Fonction exécutée avant chaque test.
+    TestBed.configureTestingModule({ // Configuration du module de test.
+      declarations: [FooterComponent] // Déclaration du FooterComponent dans le module de test.
     });
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(FooterComponent); // Création d'une instance du composant dans le fixture.
+    component = fixture.componentInstance; // Récupération de l'instance du composant.
+    fixture.detectChanges(); // Détection des changements pour initialiser le composant.
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { // Test pour vérifier si le composant est créé avec succès.
+    expect(component).toBeTruthy(); // Assertion pour s'assurer que le composant existe.
   });
 });
