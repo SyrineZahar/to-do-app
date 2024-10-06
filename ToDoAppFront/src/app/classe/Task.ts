374ca429fd013cbf83bed6db4d0ee78e1a9fd501
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 18:24:49 +0100

    Update Task.ts

--- ToDoAppFront/src/app/classe/Task.ts
+++ ToDoAppFront/src/app/classe/Task.ts
@@ -1,15 +1,18 @@
 import { TaskStatus } from "./Enum/TaskStatus.enum";
-
+/**
+ * Représente une tâche dans l'application.
+ */
 export class Task {
+  //Crée une instance de la classe Task.
     constructor(
-        public id: number,
-        public title: String,
-        public description: String,
-        public status:TaskStatus,
-        public deadline: Date,
-        public isDestactive?: boolean,
-        public createdAt?: Date, 
-        public updatedAt?: Date,
+        public id: number,// Identifiant unique
+        public title: String,// Titre de la tâche
+        public description: String, // Description de la tâche
+        public status:TaskStatus,// Statut de la tâche (enum)
+        public deadline: Date,// Date limite
+        public isDestactive?: boolean,// Indicateur de désactivation (facultatif)
+        public createdAt?: Date, // Date de création (facultatif)
+        public updatedAt?: Date,// Date de mise à jour (facultatif)
      ) {}
     
     }
