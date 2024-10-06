1040c8cddc9f3e9200d276e084a8f9f37e05bb2e
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 19:09:57 +0100

    Update task-form.component.spec.ts

--- ToDoAppFront/src/app/components/task-form/task-form.component.spec.ts
+++ ToDoAppFront/src/app/components/task-form/task-form.component.spec.ts
@@ -1,21 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; // Importation des utilitaires pour les tests unitaires.
+import { TaskFormComponent } from './task-form.component'; // Importation du composant TaskForm à tester.
 
-import { TaskFormComponent } from './task-form.component';
+describe('TaskFormComponent', () => { // Début du bloc de tests pour le TaskFormComponent.
+  let component: TaskFormComponent; // Déclaration de la variable pour l'instance du composant.
+  let fixture: ComponentFixture<TaskFormComponent>; // Déclaration de la variable pour le fixture du composant.
 
-describe('TaskFormComponent', () => {
-  let component: TaskFormComponent;
-  let fixture: ComponentFixture<TaskFormComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TaskFormComponent]
+  beforeEach(() => { // Fonction exécutée avant chaque test.
+    TestBed.configureTestingModule({ // Configuration du module de test.
+      declarations: [TaskFormComponent] // Déclaration du TaskFormComponent dans le module de test.
     });
-    fixture = TestBed.createComponent(TaskFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(TaskFormComponent); // Création d'une instance du composant dans le fixture.
+    component = fixture.componentInstance; // Récupération de l'instance du composant.
+    fixture.detectChanges(); // Détection des changements pour initialiser le composant.
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { // Test pour vérifier si le composant est créé avec succès.
+    expect(component).toBeTruthy(); // Assertion pour s'assurer que le composant existe.
   });
 });
