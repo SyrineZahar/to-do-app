5d11bc87915302e94eb6f2fc7e9decb9c0fe07a3
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 18:48:21 +0100

    Update footer.component.ts

--- ToDoAppFront/src/app/components/footer/footer.component.ts
+++ ToDoAppFront/src/app/components/footer/footer.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // Importation du décorateur Component d'Angular.
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: 'app-footer', // Nom du sélecteur utilisé pour insérer ce composant dans le HTML.
+  templateUrl: './footer.component.html', // Chemin vers le fichier HTML du template du composant.
+  styleUrls: ['./footer.component.css'] // Chemin vers le fichier CSS pour les styles du composant.
 })
-export class FooterComponent {
-
+export class FooterComponent { // Déclaration de la classe FooterComponent.
 }
