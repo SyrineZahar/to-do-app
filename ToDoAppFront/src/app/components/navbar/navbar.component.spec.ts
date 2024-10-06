5f96ee55658cbb2326b28059ca48a0579131979a
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 19:00:50 +0100

    Update navbar.component.spec.ts

--- ToDoAppFront/src/app/components/navbar/navbar.component.spec.ts
+++ ToDoAppFront/src/app/components/navbar/navbar.component.spec.ts
@@ -1,21 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'; // Importation des modules nécessaires pour effectuer des tests unitaires.
+import { NavbarComponent } from './navbar.component'; // Importation du composant Navbar à tester.
 
-import { NavbarComponent } from './navbar.component';
+describe('NavbarComponent', () => { // Début du bloc de tests pour le NavbarComponent.
+  let component: NavbarComponent; // Déclaration de la variable pour l'instance du composant.
+  let fixture: ComponentFixture<NavbarComponent>; // Déclaration de la variable pour le fixture du composant.
 
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+  beforeEach(() => { // Fonction exécutée avant chaque test.
+    TestBed.configureTestingModule({ // Configuration du module de test.
+      declarations: [NavbarComponent] // Déclaration du NavbarComponent dans le module de test.
     });
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(NavbarComponent); // Création d'une instance du composant dans le fixture.
+    component = fixture.componentInstance; // Récupération de l'instance du composant.
+    fixture.detectChanges(); // Détection des changements pour initialiser le composant.
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', () => { // Test pour vérifier si le composant est créé avec succès.
+    expect(component).toBeTruthy(); // Assertion pour s'assurer que le composant existe.
   });
 });
