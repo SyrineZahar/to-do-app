881cf87eb08c91146a367ba676243dafdd39425e
abir_yazaoui <145192358+abiryazaoui@users.noreply.github.com>
2024-10-06 17:50:31 +0100

    Update ToDoAppBackApplicationTests.java

--- ToDoAppBack/src/test/java/PI/dsi32/ToDoAppBack/ToDoAppBackApplicationTests.java
+++ ToDoAppBack/src/test/java/PI/dsi32/ToDoAppBack/ToDoAppBackApplicationTests.java
@@ -2,12 +2,14 @@ package PI.dsi32.ToDoAppBack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//Classe de test pour l'application ToDoAppBack.
 @SpringBootTest
+// Indique que le contexte de l'application doit être chargé pour les tests
 class ToDoAppBackApplicationTests {
-
+// Teste si le contexte de l'application se charge correctement.
 	@Test
 	void contextLoads() {
 	}
-
+// Ce test ne contient pas d'assertions.
+        // Il s'assure simplement que le contexte de l'application se charge sans erreur.
 }
